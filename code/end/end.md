c0d135e243466167dcd7cd683b085ae936b339b7
BlackAngle233 <740448368@qq.com>
2019-07-04 10:27:00 +0800

    add zuul & filter

--- code/end/end.md
+++ code/end/end.md
@@ -3,3 +3,5 @@
 * Use spirng cloud
   * eureka
   * feign
+  * hystrix
+  * zuul
---
+++ code/end/zuulservice/src/main/java/cn/edu/sjtu/zuulservice/DemoApplication.java
@@ -0,0 +1,19 @@
+package cn.edu.sjtu.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}

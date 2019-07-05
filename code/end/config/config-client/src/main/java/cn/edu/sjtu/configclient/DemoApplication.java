7178bc1013610678d053bd4e429f5aa0bdada1e9
BlackAngle233 <740448368@qq.com>
2019-07-05 11:07:00 +0800

    config update
    can fetch config on github

--- code/end/config/config-client/src/main/java/cn/edu/sjtu/configclient/DemoApplication.java
+++ code/end/config/config-client/src/main/java/cn/edu/sjtu/configclient/DemoApplication.java
@@ -2,8 +2,10 @@ package cn.edu.sjtu.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoApplication {
 
 	public static void main(String[] args) {

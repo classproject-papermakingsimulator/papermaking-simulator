1122442924ff763de63717da990d2f0ca1f05791
Antonatel <49440526+Antonatel@users.noreply.github.com>
2019-07-17 14:35:26 +0800

    可以砍竹子了

---
+++ code/papermaking-simulator/Assets/Scripts/BambooLock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BambooLock : MonoBehaviour
+{
+    private bool into = false;
+       
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        into = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        into = false;
+    }
+
+    public bool getInto()
+    {
+        return into;
+    }
+
+
+}
---
+++ code/papermaking-simulator/Assets/Scripts/cutBamboo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutBamboo : MonoBehaviour
+{
+
+    bambooInteract bam = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "bamboo")
+        {
+            GameObject.Find(collider.name).GetComponent<bambooInteract>().cutdown();
+        }
+    }
+
+}

5e494f0ff2989e81a9d182254cffbb48d492e668
Antonatel <49440526+Antonatel@users.noreply.github.com>
2019-07-19 11:23:16 +0800

    增加了锤子砸的震荡和物体变换脚本

---
+++ code/papermaking-simulator/Assets/Scripts/hammer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class hammer : VRTK_InteractableObject
+{
+    private float impactMagnifier = 120f;
+    private float collisionForce = 0f;
+    private float maxCollisionForce = 100f;
+    private VRTK_ControllerReference controllerReference;
+
+    public float CollisionForce()
+    {
+        return collisionForce;
+    }
+
+    public override void Grabbed(VRTK_InteractGrab grabbingObject)
+    {
+        base.Grabbed(grabbingObject);
+        controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
+    }
+
+    public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+    {
+        base.Ungrabbed(previousGrabbingObject);
+        controllerReference = null;
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        controllerReference = null;
+        interactableRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (VRTK_ControllerReference.IsValid(controllerReference) && IsGrabbed())
+        {
+            collisionForce = VRTK_DeviceFinder.GetControllerVelocity(controllerReference).magnitude * impactMagnifier;
+            var hapticStrength = collisionForce / maxCollisionForce;
+            VRTK_ControllerHaptics.TriggerHapticPulse(controllerReference, hapticStrength, 0.1f, 0.01f);
+        }
+        else
+        {
+            collisionForce = collision.relativeVelocity.magnitude * impactMagnifier;
+        }
+    }
+
+}
+
+
---
+++ code/papermaking-simulator/Assets/Scripts/transform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transform : MonoBehaviour
+{
+    public GameObject second;
+    public GameObject third;
+    public GameObject thisOne;
+    public InventoryAdd inventory;
+    private int count;
+    // Start is called before the first frame update
+    void onEnable()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        count++;
+        changeObject();
+    }
+
+    private void changeObject()
+    {
+        if(count >= 10)
+        {
+            if(thisOne.name == "Cube")
+            {
+                second.SetActive(true);
+            }
+            if (thisOne.name == "Sphere")
+            {
+                third.SetActive(true);
+            }
+            if (thisOne.name == "Capsule")
+            {
+                inventory.minus();
+                inventory.add();
+            }
+            thisOne.SetActive(false);
+        }
+    }
+}
